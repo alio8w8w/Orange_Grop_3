13c7741ddba0bd41d8979ad292787a887604de0f
alio8w8w <iboclinca89@gmail.com>
2026-07-30 22:42:00 +0300

    uPDATE

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,25 +1,8 @@
 // app/[locale]/layout.tsx
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import { NextIntlClientProvider } from 'next-intl'
 import { getMessages } from 'next-intl/server'
 import { routing } from '@/i18n/routing'
 import { notFound } from 'next/navigation'
-
-import '../globals.css'
-
-const inter = Inter({ 
-  subsets: ['latin', 'latin-ext'],
-  display: 'swap',
-})
-
-export const metadata: Metadata = {
-  title: {
-    default: 'Aplicația Mea',
-    template: '%s | Aplicația Mea',
-  },
-  description: 'Descrierea aplicației tale',
-}
 
 export default async function LocaleLayout({
   children,
@@ -39,11 +22,8 @@ export default async function LocaleLayout({
   const messages = await getMessages({ locale: activeLocale })
 
   return (
-    // Înlocuim <html> și <body> cu un container care preia fontul și atributele
-    <div className={`${inter.className} antialiased`} lang={activeLocale}>
-      <NextIntlClientProvider messages={messages} locale={activeLocale}>
-        {children}
-      </NextIntlClientProvider>
-    </div>
+    <NextIntlClientProvider messages={messages} locale={activeLocale}>
+      {children}
+    </NextIntlClientProvider>
   )
 }
